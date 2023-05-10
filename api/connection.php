cbc50d5f2eb7236c573ff2edf276196017b4edcc
plugsa <praweens62@nu.ac.th>
2023-05-10 18:37:48 +0700

    a

--- api/connection.php
+++ api/connection.php
@@ -3,8 +3,9 @@ $servername = "w-4.h.filess.io";
 $username = "Foodies_canoncemay";
 $password = "552b582b68bf54da3f6fa96f1ef9a199551415cd";
 $dbname = "Foodies_canoncemay";
+$port = "3307";
 
-    $conn = mysqli_connect($servername,$username,$password,$dbname) 
+    $conn = mysqli_connect($servername,$username,$password,$dbname,$port) 
     or die('connection failed');
 ?>
 
