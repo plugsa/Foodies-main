eeaf15444c309f56c8e8f4ae3939f8adace1fb9f
plugsa <praweens62@nu.ac.th>
2023-05-10 17:45:22 +0700

    a

--- api/connection.php
+++ api/connection.php
@@ -4,6 +4,6 @@ $username = "Foodies_canoncemay";
 $password = "552b582b68bf54da3f6fa96f1ef9a199551415cd";
 $dbname = "Foodies_canoncemay";
 
-    $conn = mysqli_connect(servername,username,password,dbname) 
+    $conn = mysqli_connect($servername,$username,$password,$dbname) 
     or die('connection failed');
 ?>
