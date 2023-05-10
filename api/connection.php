b4bfdccf92990a89bfcf6772f51317902a41f573
plugsa <praweens62@nu.ac.th>
2023-05-10 18:18:21 +0700

    x

--- api/connection.php
+++ api/connection.php
@@ -1,9 +1,12 @@
 <?php
 $servername = "w-4.h.filess.io";
-$username = "Foodies_canoncemay";
-$password = "552b582b68bf54da3f6fa96f1ef9a199551415cd";
+$username = "root";
+$password = " ";
+// $username = "Foodies_canoncemay";
+// $password = "552b582b68bf54da3f6fa96f1ef9a199551415cd";
 $dbname = "Foodies_canoncemay";
 
     $conn = mysqli_connect($servername,$username,$password,$dbname) 
     or die('connection failed');
 ?>
+
