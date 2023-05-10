c114c10f1982a363abc5c7c0e69b0ece33bc383a
plugsa <praweens62@nu.ac.th>
2023-05-10 17:04:36 +0700

    x

--- connection.php
+++ connection.php
@@ -1,4 +1,4 @@
 <?php
-    $conn = mysqli_connect("w-4.h.filess.io", "Foodies_canoncemay","552b582b68bf54da3f6fa96f1ef9a199551415cd","foodies") 
+    $conn = mysqli_connect("w-4.h.filess.io", "Foodies_canoncemay","552b582b68bf54da3f6fa96f1ef9a199551415cd","Foodies_canoncemay") 
     or die('connection failed');
 ?>
